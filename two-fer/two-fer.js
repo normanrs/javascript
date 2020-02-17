35d9c8383270f29f4835bb113fb7e91c093af00f
Norm Schultz <schultzn@aetna.com>
2020-02-17 10:57:58 -0700

    Adds two-fer

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -3,4 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = () => "One for you, one for me.";
+export const twoFer = (param) => {
+  if (param === undefined) {
+    return "One for you, one for me."
+  } else {
+    return `One for ${param}, one for me.`
+  }
+};
--- two-fer/two-fer.spec.js
+++ two-fer/two-fer.spec.js
@@ -1,7 +1,7 @@
 import { twoFer } from './two-fer'
-// import pry from 'pryjs'
+import pry from 'pryjs'
 
-// eval(pry.it)
+eval(pry.it)
 describe('twoFer()', () => {
   test('no name given', () => {
     expect(twoFer()).toEqual("One for you, one for me.")
@@ -11,7 +11,7 @@ describe('twoFer()', () => {
     expect(twoFer("Alice")).toEqual("One for Alice, one for me.")
   })
 
-  xtest('another name given', () => {
+  test('another name given', () => {
     expect(twoFer("Bob")).toEqual("One for Bob, one for me.")
   })
 })
