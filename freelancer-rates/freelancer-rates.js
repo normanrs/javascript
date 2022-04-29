f8a5eed219264f663d2a433a91c2871cbeab415f
Norman Schultz <normanrs@live.com>
2022-04-29 10:23:13 -0600

    Complete frelancer

--- freelancer-rates/freelancer-rates.js
+++ freelancer-rates/freelancer-rates.js
@@ -1,53 +1,22 @@
-// @ts-check
-//
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
+const workDayHours = 8;
+const minBillableDays = 22;
+const minBillableHours = minBillableDays * workDayHours
 
-// 👋🏽 Hi again!
-//
-// A quick reminder about exercise stubs:
-//
-// 💡 You're allowed to completely clear any stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./freelancer-rates.spec.js.
-//
-// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
-// in idiomatic JavaScript, but some companies and style-guides do enforce them.
-//
-// Get those rates calculated!
-
-/**
- * The day rate, given a rate per hour
- *
- * @param {number} ratePerHour
- * @returns {number} the rate per day
- */
 export function dayRate(ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return workDayHours * ratePerHour
 }
 
-/**
- * Calculates the number of days in a budget, rounded down
- *
- * @param {number} budget: the total budget
- * @param {number} ratePerHour: the rate per hour
- * @returns {number} the number of days
- */
 export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  const rawResult = budget/(workDayHours * ratePerHour);
+  return Math.trunc(rawResult);
 }
 
-/**
- * Calculates the discounted rate for large projects, rounded up
- *
- * @param {number} ratePerHour
- * @param {number} numDays: number of days the project spans
- * @param {number} discount: for example 20% written as 0.2
- * @returns {number} the rounded up discounted rate
- */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Remove this line and implement the function');
+  const rawHours = numDays * workDayHours;
+  const discountedRate = ratePerHour * (1 - discount)
+  const discountMonths = Math.trunc(rawHours / minBillableHours)
+  const discountHours = discountMonths * minBillableDays * workDayHours
+  const discountTotal = discountHours * discountedRate
+  const remainingTotal = (rawHours - discountHours) * ratePerHour
+  return Math.ceil(discountTotal + remainingTotal);
 }
